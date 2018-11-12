3ba408248581dc74cf1875531da6467af00673fe
Emily Zou <manshizou2018@u.northwestern.edu>
2018-11-12 14:55:53 -0500

    1

---
+++ Documents/Linechecker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+count=$(cat $1 | wc -l)
+
+
+function numlines(){
+	
+	if [[ $count -eq 0 ]]; then
+		echo file is empty
+	elif [[ $count -lt 10 ]]; then
+		echo file has less than or equal to 10 lines
+	else
+		echo file has more than 10 lines
+
+	fi
+
+}
+
+echo $(numlines $count)
